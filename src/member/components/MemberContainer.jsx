444c1c64cc789322f7e99a11e9a2e297c1eb6768
Ephraim Gregor <ephraim@ephraimgregor.com>
2016-07-19 00:24:51 -0400

    Provides more consistent way to get member ID.

--- src/member/components/MemberContainer.jsx
+++ src/member/components/MemberContainer.jsx
@@ -2,17 +2,17 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Member, MemberEdit } from './';
 import { fetchMemberIfNeeded } from '../actions';
-import { getMemberById, getMemberDomain } from '../selectors';
+import { getCurrentId, getMemberById, getMemberDomain } from '../selectors';
 import { Loading } from '../../shared';
 
 class MemberContainer extends React.Component {
 	componentDidMount() {
 		const { dispatch } = this.props;
-		dispatch( fetchMemberIfNeeded( this.id ) );
+		dispatch( fetchMemberIfNeeded( this.props.memberId ) );
 	}
 
 	render() {
-		if ( ! this.props.member ) {
+		if ( ! Object.keys( this.props.member ).length ) {
 			return( <Loading /> );
 		} else if ( this.props.route.editMode ) {
 			return( <MemberEdit member={ this.props.member } /> );
@@ -20,19 +20,19 @@ class MemberContainer extends React.Component {
 			return( <Member member={ this.props.member } domain={ this.props.domain } /> );
 		}
 	}
-
-	get id() {
-		return this.props.params.id;
-	}
 }
 
 MemberContainer.propTypes = {
+	memberId: PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]).isRequired,
 	member:   PropTypes.object.isRequired,
 	domain:   PropTypes.object,
 	dispatch: PropTypes.func.isRequired
 }
 
-export default connect( ( state, props ) => ({
-	member: getMemberById( state, props ),
-	domain: getMemberDomain( state, props )
-}) )( MemberContainer );
+export default connect( ( state, props ) => {
+	return {
+		memberId: getCurrentId( state, props ),
+		member:   getMemberById( state, props ),
+		domain:   getMemberDomain( state, props )
+	};
+})( MemberContainer );
--- src/member/selectors.js
+++ src/member/selectors.js
@@ -1,16 +1,19 @@
 import { createSelector } from 'reselect';
 import { Map } from 'immutable';
 
-const memberById = ( state, props ) => {
-	let id      = props.params.id;
-	let members = state.member.items;
-
-	return members.get( id ) || members.find( m => id === m.get( 'membershipNumber' ) );
-};
+export const getCurrentId = ( state, props ) => {
+	let id = props.route.id || props.params.id;
+	return Number.parseInt( id, 10 ) || id;
+}
 
 export const getMemberById = createSelector(
-	memberById,
-	( member = {} ) => member
+	getCurrentId,
+	state => state.member.items,
+	( id, members ) => (
+		members.get( id ) ||
+		members.find( m => id === m.get( 'membershipNumber' ) ) ||
+		{}
+	)
 );
 
 export const getMemberList = createSelector(
@@ -21,7 +24,7 @@ export const getMemberList = createSelector(
 export const getMemberDomain = createSelector(
 	getMemberById,
 	state => state.domain.items,
-	( member, domains ) => {
+	( member = {}, domains ) => {
 		if ( ! Object.keys( member ).length ) {
 			return new Map();
 		}
